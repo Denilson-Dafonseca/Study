b331c7eb96442f53927cd4a1800ac89f15c4c1f9
Denilson Dafonseca <denilkson.dafonseca99@gmail.com>
2026-06-05 14:49:13 +0200

    fix

--- class/settings.py
+++ class/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mcsn0(5y4$9nq24-_uhggdbtnxy02it(j57&g#tzr^bmj2j4!*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://github.com/enterprises/we-study"]
+ALLOWED_HOSTS = ["https://musical-kitsune-fb262f.netlify.app"]
 
 
 # Application definition
